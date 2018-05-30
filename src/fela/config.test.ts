7072a1a5aa0d7a7266a635df83806864cb67fe34
Matthew Wagerfield <matthew@wagerfield.com>
2018-05-30 21:36:56 +0100

    updated test name

--- src/fela/config.test.ts
+++ src/fela/config.test.ts
@@ -2,7 +2,7 @@ import { createRenderer } from "fela"
 import createConfig from "./config"
 
 describe("createConfig(options)", () => {
-  it("returns an object", () => {
+  it("returns a config object", () => {
     expect(createConfig()).toEqual({
       enhancers: expect.any(Array),
       plugins: expect.any(Array)
