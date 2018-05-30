0f74f22028ce7203c3370169ef1319f4fcfb70d2
Matthew Wagerfield <matthew@wagerfield.com>
2018-05-30 16:42:21 +0100

    added flex rule export to rules index

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,7 @@
 export { default as border } from "./border/border"
 export { default as disc } from "./disc/disc"
 export { default as display } from "./display/display"
-export { default as edges } from "./edges/edges"
+export { default as edge } from "./edge/edge"
 export { default as ellipsis } from "./ellipsis/ellipsis"
 export { default as fill } from "./fill/fill"
+export { default as flex } from "./flex/flex"
